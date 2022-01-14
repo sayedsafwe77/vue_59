2ef69bcf71762680e01d431eab2ecd14af234982
sayedsafwet77 <sayedsafwet4@gmail.com>
2022-01-14 13:19:36 +0200

    vue 59

---
+++ src/components/Category.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>hello {{ user_name }}</h1>    
+</template>
+<script>
+export default {
+    name: 'Category',
+    props:{
+        user_name:String
+    }
+}
+</script>
---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>hello from profile page</h1>
+    <!-- <HelloWorld msg="hello mohamed salah"/> -->
+    <Category user_name='mohamed salah'/>
+    <Category user_name='sayed safwet'/>
+</template>
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+import Category from '@/components/Category.vue'
+export default {
+      name: 'Profile',
+      components:{
+          HelloWorld,
+          Category
+      }
+}
+</script>
